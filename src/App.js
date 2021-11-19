856531b0a7531a3e19e84bbe44adba59b6ee59e8
cynthia.omovoiye <omovoiye@gmail.com>
2021-11-19 16:57:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const dictionaryFunction = async ()=>{
+    
+    try{
+      const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/hello')
+console.log(data)
+    }
+    catch(error){
+      console.log(error)
+
+    }
+
+  }
+  useEffect(()=>{
+    dictionaryFunction()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
